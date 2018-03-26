07b0e8f262aa57f4170675ace3cbcbd2f47785e1
James Hortman <36178931+Jhortman@users.noreply.github.com>
2018-03-26 10:40:27 -0500

    Update ClientGUI.java

--- ClientGUI.java
+++ ClientGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,10 +11,19 @@ public class ClientGUI extends JFrame {
 	private JTextField _lastName;
 	private JTextField _department;
 	private JTextField _phone;
+	private JRadioButton _male;
+	private JRadioButton _female;
+	private JRadioButton _other;
+	private JList _list;
+    private DefaultListModel<String> _listModel;
+	private JButton _submit = new JButton("Submit");
+	private JButton _exit = new JButton("Exit");
+	Directory dir = new Directory();
+	
 	
 	public ClientGUI(){
 		setTitle("Client");
-        setSize(500, 500);
+        setSize(500,200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
         
@@ -23,71 +33,110 @@ public class ClientGUI extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
 	}
+	@SuppressWarnings("unchecked")
 	public void createDisplay(){
 		
-		JPanel jOne = new JPanel();
-		jOne.setLayout(new GridLayout(10,1));
-		add(jOne,BorderLayout.WEST);
-		jOne.setSize(200,200);
+		//Labels
+		JLabel fName, lName, deptName, phone, genderM, genderF, genderOth;
+		fName = new JLabel("First Name: ");
+		lName = new JLabel("Last Name: ");
+		deptName = new JLabel("Department Name: ");
+		phone = new JLabel("Phone: ");
+		genderM = new JLabel("Male");
+		genderF = new JLabel("Female");
+		genderOth = new JLabel("Other");
+		_male = new JRadioButton();
+		_female = new JRadioButton();
+		_other = new JRadioButton();
 		
+		ButtonGroup group = new ButtonGroup();
+		group.add(_male);
+		group.add(_female);
+		group.add(_other);
 		
-		jOne.add(new JLabel("First Name"));
+		JPanel submitExitPanel = new JPanel(new GridLayout(1,2));
+		JPanel entryPanel = new JPanel(new GridLayout(4,2));
+		JPanel typePanel = new JPanel(new GridLayout(4,2));
+		JPanel type2Panel = new JPanel(new BorderLayout());
+		
 		_firstName = new JTextField(20);
-		_firstName.getText();
-		jOne.add(_firstName);
-		
-		jOne.add(new JLabel("Last Name"));
 		_lastName = new JTextField(20);
-		jOne.add(_lastName);
-		
-		jOne.add(new JLabel("Department Name"));
 		_department = new JTextField(20);
-		jOne.add(_department);
-		
-		jOne.add(new JLabel("Phone"));
 		_phone = new JTextField(20);
-		jOne.add(_phone);
 		
+		//Add buttons to button panel
+		submitExitPanel.add(_submit);
+		submitExitPanel.add(_exit);
+		//Add Fields of entry information to entry panel
+		entryPanel.add(fName);
+		entryPanel.add(_firstName);
 		
-		for(int i = 0; i < 6; i++){
-			jOne.add(new JLabel(""));
-		}
+		entryPanel.add(lName);
+		entryPanel.add(_lastName);
 		
+		entryPanel.add(deptName);
+		entryPanel.add(_department);
+	
+		entryPanel.add(phone);
+		entryPanel.add(_phone);
 		
+		//Add Person type to type panel
+		typePanel.add(genderM);
+		typePanel.add(_male);
 		
+		typePanel.add(genderF);
+		typePanel.add(_female);
 		
+		typePanel.add(genderOth);
+		typePanel.add(_other);
 		
+		//List Title (Mr./Ms./Mrs./Dr./Col./Prof.) Selection number: 1
+		_listModel= new DefaultListModel<String>();
+		_listModel.addElement("Ms.");
+		_listModel.addElement("Mrs.");
+		_listModel.addElement("Dr.");
+		_listModel.addElement("Col.");
+		_listModel.addElement("Prof.");
+			
+		_list = new JList<String>(_listModel); //data has type Object[]
+		_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		_list.setLayoutOrientation(JList.VERTICAL);
+		_list.setVisibleRowCount(-1);
 		
+				
+		type2Panel.add(_list,BorderLayout.NORTH);
 		
+		//Add panels to Main Panel
+		add(entryPanel,BorderLayout.CENTER);
+		add(submitExitPanel,BorderLayout.SOUTH);
+		add(typePanel,BorderLayout.WEST);
+		add(type2Panel,BorderLayout.EAST);
 		
+		_submit.addActionListener(new ClickListener());
+		_exit.addActionListener(new ClickListener());
 		
-		
-	/*		JPanel jTwo = new JPanel();
-		jOne.setLayout(new GridLayout(2,2));
-		add(jTwo,BorderLayout.SOUTH);
-		
-		jTwo.add(new JButton());
-		jTwo.add(new JMenu());
-		jTwo.add(new JButton());
-		jTwo.add(new JButton());
-		
-		
-		
-		*/
-		
-		
-		
+
 	}
 	
-	public class TextListener implements ActionListener{
-		private String name;
-		
-		public TextListener(String name){
-			
-		}
-	
-		public void actionPerformed(ActionEvent e){
-			
+	class ClickListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent event) 
+		{
+			if(event.getSource().equals(_submit))
+			{
+				Employee newEmp = new Employee(_lastName.getText(),_firstName.getText(),_phone.getText(),_department.getText());
+				dir.add(newEmp);
+				_lastName.setText("");
+				_firstName.setText("");
+				_phone.setText("");
+				_department.setText("");
+				System.out.println("Employee Added");
+			}
+			if(event.getSource().equals(_exit))
+			{
+				System.exit(0);
+			}
 			
 		}
 	
@@ -95,6 +144,7 @@ public class ClientGUI extends JFrame {
 	public static void main(String[] args){
 		
 		new ClientGUI();
+		
 		
 	}
 
